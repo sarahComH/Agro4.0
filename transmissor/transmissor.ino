ac0672caec79206cc6de94b58074c4f9701cf051
sarahComH <106473736+sarahComH@users.noreply.github.com>
2022-12-07 14:23:06 -0300

    Update transmissor.ino

--- transmissor/transmissor.ino
+++ transmissor/transmissor.ino
@@ -4,17 +4,17 @@
 
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
-int dado = 0;
+int dado = 0; // variável que será tranmitida
 void setup() {
   radio.begin();
-  radio.openWritingPipe(address);
+  radio.openWritingPipe(address); // abrindo o caminho para a transmissão
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
 }
 void loop() {
   
   dado++;
-  radio.write(&dado, sizeof(dado));
+  radio.write(&dado, sizeof(dado)); // transmite o dado
 
   delay(1000);
 }
